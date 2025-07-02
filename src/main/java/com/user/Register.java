3a1491ddda56d31dbb7d7e2522cdde54196f9248
sweta <sg8528224@gmail.com>
2025-07-02 14:45:37 +0530

    Image processing added

--- src/main/java/com/user/Register.java
+++ src/main/java/com/user/Register.java
@@ -3,14 +3,21 @@ package com.user;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.DriverManager;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+import jakarta.servlet.annotation.MultipartConfig;
 
+
+@MultipartConfig
 public class Register extends HttpServlet {
 
     protected void processRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -22,10 +29,13 @@ public class Register extends HttpServlet {
             String name = req.getParameter("user_name");
             String password = req.getParameter("user_password");
             String email = req.getParameter("user_email");
+            Part part=req.getPart("image");
+            String filename=part.getSubmittedFileName();
+           // out.println(filename);
 
            // out.print(name + password + email);
 
-            // connection
+            // connection............................
             try {
             	Thread.sleep(3000);
                 Class.forName("com.mysql.cj.jdbc.Driver");
@@ -33,14 +43,29 @@ public class Register extends HttpServlet {
                 Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/register_Servlet", "root", "@0108madhav");
 
                 // query
-                String q = "insert into user_data (name , password, email) values(?, ?, ?)";
+                String q = "insert into user_data (name , password, email,imagename) values(?, ?, ?,?)";
 
                 PreparedStatement pstmt = conn.prepareStatement(q);
                 pstmt.setString(1, name);
                 pstmt.setString(2, password);
                 pstmt.setString(3, email);
+                pstmt.setString(4, filename);
 
                 pstmt.executeUpdate();
+                
+                //upload file..............
+                InputStream is=part.getInputStream();
+                byte []data=new byte[is.available()];
+                
+                is.read(data);
+                String path =  getServletContext().getRealPath("/")+"img"+File.separator+filename;
+              // out.println(path);
+                
+                FileOutputStream fout=new FileOutputStream(path);
+                fout.write(data);
+                fout.close();
+                
+                
 
                 out.println("Done");
 
